8d128538ddbe6aa08200e4eb238b42729483df39
alas129 <alas129@163.com>
2024-03-19 23:41:25 -0700

    Added bucket sort and swap pairs

--- SwapPairs.java
+++ SwapPairs.java
@@ -10,7 +10,14 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class SwapPairs {
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+  class SwapPairs_Solution {
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) {
             return head;
@@ -23,6 +30,14 @@ class SwapPairs {
         head.next = swapPairs(thirdNode);
 
         return secondNode;
+    }
+}
+
+public class SwapPairs {
+    public static void main(String[] args) {
+        SwapPairs_Solution solutio = new SwapPairs_Solution();
+
+        //test
     }
 }
 
