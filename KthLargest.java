60f20a79af328a4767e98f327541647f5f246ad7
alas129 <alas129@163.com>
2024-04-08 23:27:28 -0700

    Kth Largest with priority queue

---
+++ KthLargest.java
@@ -0,0 +1,34 @@
+import java.util.PriorityQueue;
+
+class KthLargest {
+    private int k;
+    private PriorityQueue<Integer> minHeap;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>(k);
+
+        // Insert initial elements from nums into minHeap
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.offer(val);
+        } else if (val > minHeap.peek()) {
+            minHeap.poll(); // Remove the smallest element (root)
+            minHeap.offer(val); // Add val to minHeap
+        }
+
+        // Return the current kth largest element which is the root of minHeap
+        return minHeap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
